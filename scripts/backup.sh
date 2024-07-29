8b9c0af824f5e1b27827f1d1903a615d9dee89cd
Thomas Boni <thomas@r2devops.io>
2024-07-29 12:04:37 +0200

    conf(backup&restore): remove minio and cleanup files

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,66 +2,112 @@
 
 # This script creates a backup of your data inside an archive.
 # Launch it in the docker compose folder
-# Usage: ./backup.sh
 
+# Usage function
+usage() {
+    echo "Usage: $0 pg-version [s3-destination] [s3-endpoint] [s3-access-key] [s3-secret-key] [s3-region]"
+    echo "Backup R2Devops. You must run this CLI from the root of your R2Devops local git repository"
+    echo
+    echo "Options:"
+    echo "  pg-version      Version of PostgreSQL you are using"
+    echo "  s3-destination  Name of the S3 bucket and path to upload the backup (s3://bucket-name/folder)"
+    echo "  s3-endpoint     S3 endpoint (https://s3-endpoint-url)"
+    echo "  s3-region       S3 bucket region"
+    echo "  s3-access-key   S3 Access Key ID"
+    echo "  s3-secret-key   S3 Secret Access Key"
+    echo "  -h, --help      Display this help message"
+}
+
+# Source R2Devops configuration
 source .env
 
+# Define some consts
 BACKUPS_DIR=backups
 BACKUP_NAME=backup_r2-$(date +%Y-%m-%d_%H-%M-%S)
 BACKUP_PATH=$BACKUPS_DIR/$BACKUP_NAME
-
 PROJECT_NAME=r2devops
 
-ALPINE_VERSION=3.16
-PG_VERSION=13
+# Check if --help or -h is provided as the first argument
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+    usage
+    exit 0
+fi
+
+# Get arguments
+PG_VERSION=$1
+S3_DESTINATION=$2
+S3_ENDPOINT=$3
+S3_REGION=$4
+S3_ACCESS_KEY=$5
+S3_SECRET_KEY=$6
 
 # Create the backup directory
 mkdir -p $BACKUP_PATH
-echo "📦 Start backuping data..."
+echo "📦 Start backing up data..."
 
-# Backup the Jobs Database
-echo "Saving the jobs database... (1/5)"
-if docker run --rm --network=${PROJECT_NAME}_intranet -v $PWD/$BACKUP_PATH:/backup -e PGPASSWORD=$JOBS_DB_PASSWORD -it postgres:$PG_VERSION /bin/bash -c "pg_dump -U jobs -h postgres -Ft -f /jobs_db_backup.tar && mv jobs_db_backup.tar /backup"; then
-    echo "✅ jobs database has been saved"
+# Backup the database
+echo "Saving the database..."
+if docker run --rm --network=${PROJECT_NAME}_intranet -v $PWD/$BACKUP_PATH:/backup -e PGPASSWORD=$JOBS_DB_PASSWORD postgres:$PG_VERSION /bin/bash -c "pg_dump -U $JOBS_DB_USER -h $JOBS_DB_HOST -Ft -f /db_backup.tar && mv db_backup.tar /backup"; then
+    echo "✅ Database has been saved"
 else
-    echo "❌ Error while saving jobs database"
-fi
-
-# Backup the Minio bucket
-echo "Saving the Minio bucket... (2/5)"
-if docker run --rm --network=${PROJECT_NAME}_intranet --volumes-from ${PROJECT_NAME}-minio-1 -v $PWD/$BACKUP_PATH:/backup -it alpine:$ALPINE_VERSION /bin/sh -c "cd /export/$S3_BUCKET && tar cf /backup/minio_backup.tar *"; then
-    echo "✅ Minio bucket has been saved"
-else
-    echo "❌ Error while saving Minio bucket"
-fi
-
-# Backup the certificate file for Traefik
-echo "Saving the certificate... (3/5)"
-if docker run --rm --network=${PROJECT_NAME}_intranet --volumes-from ${PROJECT_NAME}-traefik-1 -v $PWD/$BACKUP_PATH:/backup -it alpine:$ALPINE_VERSION /bin/sh -c "cp /acme/acme.json /backup"; then
-    echo "✅ The certificate has been saved"
-else
-    echo "❌ Error while saving the certificate"
+    echo "❌ Error while saving database"
+    exit 1
 fi
 
 # Backup the `.env` file:
-echo "Saving the .env file... (4/5)"
+echo "Saving the .env file..."
 if cp .env $BACKUP_PATH; then
     echo "✅ The .env file has been saved"
 else
     echo "❌ Error while saving the .env file"
+    exit 1
 fi
 
 # Backup the `config.json` file:
-echo "Saving the config.json file... (5/5)"
-if cp config.json $BACKUP_PATH; then
+echo "Saving the config.json file..."
+if cp .docker/r2devops/config.json $BACKUP_PATH; then
     echo "✅ The config.json file has been saved"
 else
     echo "❌ Error while saving the config.json file"
+    exit 1
 fi
 
-# Generate an archive of all file with the date of the day
-cd $BACKUPS_DIR
-tar cf $BACKUP_NAME.tar $BACKUP_NAME
-cd ..
-rm -rf $BACKUP_PATH
-echo "✨ Backup completed, archive: $BACKUP_PATH.tar"
+# Create a tar.gz archive of the backup
+echo "Archiving the backup..."
+if tar -czf $BACKUP_PATH.tar.gz -C $BACKUP_PATH .; then
+    echo "✅ Backup has been archived"
+else
+    echo "❌ Error while archiving backup"
+    exit 1
+fi
+
+# Optional: Upload the backup to S3 if the bucket name is provided
+if [ -n "$S3_DESTINATION" ]; then
+    echo "Uploading the backup to S3..."
+
+    # Configure AWS credentials temporarily
+    export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY
+    export AWS_SECRET_ACCESS_KEY=$S3_SECRET_KEY
+
+    # Remove the "s3://" prefix from S3_DESTINATION if it exists
+    S3_DESTINATION_CLEANED=$(echo $S3_DESTINATION | sed 's/^s3:\/\///')
+
+    # Construct the S3 URI
+    S3_URI="s3://$S3_DESTINATION_CLEANED/$BACKUP_NAME.tar.gz"
+
+    # Prepare the AWS CLI command
+    S3_CLI_CMD="aws s3 cp $BACKUP_PATH.tar.gz $S3_URI --region $S3_REGION --endpoint-url $S3_ENDPOINT"
+
+    # Execute the AWS CLI command
+    if eval $S3_CLI_CMD; then
+        echo "✅ Backup has been uploaded to S3 bucket $S3_DESTINATION at $S3_URI"
+    else
+        echo "❌ Error while uploading backup to S3"
+    fi
+
+    # Unset AWS credentials after use
+    unset AWS_ACCESS_KEY_ID
+    unset AWS_SECRET_ACCESS_KEY
+else
+    echo "⏭️ Skipping S3 upload as AWS credentials or bucket name were not provided."
+fi
